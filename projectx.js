9b14c7d79d37693acda7104030a234129db7630f
Urdo <karol.ugarenko@gmail.com>
2022-02-07 20:21:35 +0100

    fix switch

--- projectx.js
+++ projectx.js
@@ -4,28 +4,46 @@ let blue = 100;
 let key = 0;
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`; // gery backgroundColor
 
+// // IF 
+// const changeColor = (e) => {
+//    console.log(e.keyCode); 
+//    let key = e.keyCode;
+// if (key === 38){
+//     if (red < 255){
+//      red = red + 10;
+//      green = green + 10;
+//     blue = blue + 10;
+//     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+// }}
+// if (key === 40){
+//     if(red > 0){
+//     red = red - 10;
+//     green = green - 10;
+//    blue = blue - 10;
+//    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+// }}
+
+
+// }
+
+//SWITCH
 const changeColor = (e) => {
-   console.log(e.keyCode); 
-   let key = e.keyCode;
-if (key === 38){
-    if (red < 255){
-     red = red + 10;
-     green = green + 10;
-    blue = blue + 10;
-    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-}}
-if (key === 40){
-    if(red > 0){
-    red = red - 10;
-    green = green - 10;
-   blue = blue - 10;
-   document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-}}
-
-
+switch (e.keyCode) {
+    case 38:
+        red++;
+        green++;
+        blue++;
+        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        break;
+    case 40:
+        red--;
+        green--;
+        blue--;
+        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        break;
 }
 
-
+}
 // keyUP - 38
 // keyDown - 40
 
