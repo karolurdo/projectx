4e76bc1fc33b6704e7e8ff64d910ec0f928492a6
Urdo <karol.ugarenko@gmail.com>
2022-02-07 19:52:43 +0100

    check information about keyboard

--- projectx.js
+++ projectx.js
@@ -1,1 +1,14 @@
-const a = a;
+let red = 100;
+let green = 100;
+let blue = 100;
+
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+const changeColor = (e) => {
+    console.log(e.keyCode);
+}
+
+// keyUP - 38
+// keyDown - 40
+
+window.addEventListener('keydown', changeColor)
