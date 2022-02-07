d93ea0b08eb4baad72497f3ec347ae1a10c5794f
Urdo <karol.ugarenko@gmail.com>
2022-02-07 20:14:44 +0100

    fix rgb

--- projectx.js
+++ projectx.js
@@ -8,17 +8,19 @@ const changeColor = (e) => {
    console.log(e.keyCode); 
    let key = e.keyCode;
 if (key === 38){
+    if (red < 255){
      red = red + 10;
      green = green + 10;
     blue = blue + 10;
     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-}
+}}
 if (key === 40){
+    if(red > 0){
     red = red - 10;
     green = green - 10;
    blue = blue - 10;
    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-}
+}}
 
 
 }
